a47aa62ac48011b94ddad29f06b0667a8e1b53e6
OnrrGvnn <onrr.gvnn@gmail.com>
2024-12-10 19:26:46 +0300

    Repo Structure created and first two project completed.

---
+++ src/BaseProblem.java
@@ -0,0 +1,28 @@
+public class BaseProblem {
+    private int problemNumber;
+    private String result;
+
+    BaseProblem() {
+
+    }
+
+    BaseProblem(int problemNumber) {
+        this.problemNumber = problemNumber;
+    }
+
+    public int getProblemNumber() {
+        return problemNumber;
+    }
+
+    public void setProblemNumber(int problemNumber) {
+        this.problemNumber = problemNumber;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,27 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        String result;
+
+        List<BaseProblem> problems = new ArrayList<>();
+
+        MultiplesOfThreeOrFive multiplesOfThreeOrFive = new MultiplesOfThreeOrFive();
+        result = multiplesOfThreeOrFive.solve();
+        multiplesOfThreeOrFive.setResult(result);
+        problems.add(multiplesOfThreeOrFive);
+
+        EvenFibonacciNumbers evenFibonacciNumbers = new EvenFibonacciNumbers();
+        result = evenFibonacciNumbers.solve();
+        evenFibonacciNumbers.setResult(result);
+        problems.add(evenFibonacciNumbers);
+
+        System.out.println();
+
+        for (BaseProblem problem : problems) {
+            System.out.println(problem.getProblemNumber() + " - " + problem.getClass().getSimpleName() + " : " + problem.getResult());
+        }
+
     }
 }
